b97503ebb31749691b76afb99e455597dd9c5562
Shakil Ahmed Sumon <ashakil007@gmail.com>
2019-02-04 16:24:30 +0600

    Code

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # sentiment_analysis
 
-Bangla Sentiment Analysis
+Bangla Sentiment Analysis Using Long Short Term Memory Network with Attention Mechanism.
 
 Understanding the sentiment of facebook comments.
+
+Three types of Sentiment:
+
+1. Positive
+2. Negative
+3. Neutral
+
+
 
--- new_version/curriculam_preprocess.py
+++ new_version/curriculam_preprocess.py
@@ -18,7 +18,33 @@ def read_file(DATA_PATH):
 	return y, bangla_text
 
 
+def char_to_utf(bangla_text, max_len):
+	utf_all = []
+
+	for text in bangla_text:
+		for string in text:
+			utf = []
+			for char in string:
+				number = ord(char)
+				utf.append(number)
+			utf = np.asarray(utf)
+			if(utf.shape[0]>max_len):
+				utf = utf[:max_len]
+			else:
+				pad_width = max_len - utf.shape[0]
+				utf=np.pad(utf,pad_width=(0,pad_width),mode='constant')
+			
+			utf_all.append(utf)
+	utf_all=np.asarray(utf_all)
+	return utf_all
+
+			
+
+
 if __name__ == '__main__':
 	y, bangla_text = read_file(DATA_PATH)
-	print(y)
-	print(bangla_text)
+	utf_all = char_to_utf(bangla_text, 100)
+	#print(y)
+	#print(bangla_text)
+
+	print(utf_all)
